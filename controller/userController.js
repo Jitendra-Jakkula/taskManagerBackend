80cecb5e33e1bc35b21b3ea3d7b68ee0413f7c66
Your Name <your.email@example.com>
2026-01-01 23:32:42 +0530

    added controllers for login and signup

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,3 @@
-const express = require("express");
 const User = require("../models/user-model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
@@ -81,4 +80,7 @@ const login = async (req, res) => {
     }
 }
 
-
+module.exports = {
+    signUp,
+    login
+};
